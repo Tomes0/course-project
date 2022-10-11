c599bf22568b70fe9582710f6066c87ac14c7b1e
kutlak.tamas <kutlakt@yahoo.com>
2022-10-11 13:39:55 +0200

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recepies', component: RecepiesComponent},
+  { path: 'shoppinglist', component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
